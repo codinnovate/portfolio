e300f189e5a0117bf2e51ecdc314a0cb8fbd325a
samuelcody710 <adeyemis710@gmail.com>
2023-08-11 19:12:30 +0100

    added pitch

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,18 +6,23 @@ import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
-import { LanguageOutlined } from "@mui/icons-material";
+import { CallMissedOutgoingOutlined, LanguageOutlined } from "@mui/icons-material";
+import TextShort from "./TextShort";
+
 
 const ProjectCard = ({
-  index,
   name,
   description,
   tags,
   image,
   source_code_link,
 }) => {
+
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <>
+      <motion.div
+        // variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+      >
       <div
         className='bg-white p-5 select-none rounded-2xl sm:w-[360px] w-full'
       >
@@ -70,7 +75,9 @@ const ProjectCard = ({
           ))}
         </div>
       </div>
-    </motion.div>
+      </motion.div>
+          </>
+
   );
 };
 
@@ -84,14 +91,20 @@ const Works = () => {
 
       <div className='w-full flex'>
         <motion.h2
-          variants={fadeIn("", "", 0.1, 1)}
+          // variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-white text-xl max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
+          <TextShort
+            text1="Following projects showcases my skills and experience through
+          real-world examples of my work."
+            text2="Each project is briefly described with
+          links to code repositories and live demos in it."
+            text3="It reflects my
           ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          and manage projects effectively."
+          
+          />
+            
         </motion.h2>
       </div>
 
@@ -99,6 +112,54 @@ const Works = () => {
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
+      </div>
+      <div className="flex flex-col mt-20">
+        <div className="flex flex-col ">
+        <div className="flex flex-col">
+           <motion.div >
+            <p className={`${styles.sectionSubText} `}>ALX - GROUP 1197: THE NERD AVENGERS 🤓🥋</p>
+            <h2 className={`${styles.sectionHeadText}`}>QUIZZAR</h2>
+            </motion.div>
+          <div className=" max-w-[400] md:max-w-[500] min-h-[660px]">
+            <iframe width="100%" height="100%"
+              src="https://www.youtube.com/embed/MEN8KfYP9BQ"
+              title="YouTube video player" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; 
+              encrypted-media; gyroscope;
+            picture-in-picture; web-share" allowfullscreen>
+
+              </iframe>
+                 <motion.h1
+          // variants={fadeIn("", "", 0.1, 1)}
+          className='mt-5 text-white'
+              >
+                <TextShort
+                text1="  In Nigeria's education system, examination malpractice poses a significant challenge with far-reaching consequences.
+                Recent data from the West African Examinations Council (WAEC) reveals that out of 1,061,047 candidates who sat for the exam in 2022, the results of 365,564 candidates were withheld due to malpractice,
+                indicating the widespread nature of the problem."
+                text2=" This not only undermines the credibility of the education system but also results in substantial financial losses for parents and state governments.
+                Moreover, the rising registration fees for examinations, reaching NGN 18,000 in 2021, place students and their families at a daunting financial risk,
+                deterring many from investing in exams due to the high likelihood of failure and subsequent financial loss.
+                "
+                  text3=" This perpetuates a culture of malpractice as students resort to desperate measures to pass at any cost,
+                hindering access to educational opportunities and compromising the integrity of the assessment process.
+                To address this complex challenge, a comprehensive solution is required that alleviates the fear of failure and addresses the root causes of malpractice.
+       "
+                />
+              
+               </motion.h1>
+            <button className="text-white bg-[#e91e63] hover:text-[#e91e63] p-4 mr-3 my-4 text-xl items-center rounded-full font-medium hover:bg-white max-w-[15em] transition ">
+              <a _blank href=" https://docs.google.com/presentation/d/1XrOski7un5izDvxdseJUwVQOg6oRqeIbphZPgRHL78Y/edit?usp=sharing">Project Slide Deck Here</a>
+              <CallMissedOutgoingOutlined fontSize="large"  className="ml-3 hover:text-[#e91e63]"/>
+         </button>
+          </div>
+            
+        </div>
+        
+        </div>
+        
+
+
       </div>
     </>
   );
